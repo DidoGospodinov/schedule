80e68e9f0aa4cab51f9563f91d2b3a84793ea31b
DidoGospodinov <115457893+DidoGospodinov@users.noreply.github.com>
2024-12-29 17:06:45 +0200

    feat: added schedule generator

--- database/queries.py
+++ database/queries.py
@@ -1,79 +1,187 @@
-from datetime import datetime, timedelta
+import os
+from calendar import monthrange
+from datetime import datetime, timedelta, date
 import sqlite3
-from typing import List, Tuple
 
 
-class Database:
-    def __init__(self, db_path: str = 'schedule_database.db'):
-        self.conn = sqlite3.connect(db_path)
-        self.cursor = self.conn.cursor()
-        self.setup_database()
+db_path = os.path.join(os.path.dirname(__file__), 'schedule_database.db')
 
-    def setup_database(self):
-        self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS teams (
-                team_id INTEGER PRIMARY KEY,
-                team_name TEXT NOT NULL
-            )
-        ''')
 
-        self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS team_members (
-                member_id INTEGER PRIMARY KEY,
-                team_id INTEGER,
-                first_name TEXT NOT NULL,
-                last_name TEXT NOT NULL,
-                role TEXT NOT NULL,
-                FOREIGN KEY (team_id) REFERENCES teams (team_id)
-            )
-        ''')
+def setup_database():
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
 
-        self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS shift_types (
-                shift_type_id INTEGER PRIMARY KEY,
-                name TEXT NOT NULL,
-                start_time TEXT NOT NULL,
-                end_time TEXT NOT NULL
-            )
-        ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS teams (
+            team_id INTEGER PRIMARY KEY,
+            team_name TEXT NOT NULL
+        )
+    ''')
 
-        self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS schedule (
-                schedule_id INTEGER PRIMARY KEY,
-                team_id INTEGER,
-                shift_type_id INTEGER,
-                date DATE NOT NULL,
-                FOREIGN KEY (team_id) REFERENCES teams (team_id)
-                FOREIGN KEY (shift_type_id) REFERENCES shift_types (shift_type_id)
-            )
-        ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS team_members (
+            member_id INTEGER PRIMARY KEY,
+            team_id INTEGER,
+            first_name TEXT NOT NULL,
+            last_name TEXT NOT NULL,
+            role TEXT NOT NULL,
+            FOREIGN KEY (team_id) REFERENCES teams (team_id)
+        )
+    ''')
 
-        self.cursor.execute('''
-                    CREATE TABLE IF NOT EXISTS member_shifts (
-                        member_shift_id INTEGER PRIMARY KEY,
-                        schedule_id INTEGER,
-                        member_id INTEGER,
-                        actual_start_time DATETIME,
-                        actual_end_time DATETIME,
-                        notes TEXT,
-                        FOREIGN KEY (schedule_id) REFERENCES schedule (schedule_id),
-                        FOREIGN KEY (member_id) REFERENCES team_members (member_id)
-                    )
-                ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS shift_types (
+            shift_type_id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            start_time TEXT NOT NULL,
+            end_time TEXT NOT NULL
+        )
+    ''')
 
-        self.cursor.execute('''
-                    CREATE TABLE IF NOT EXISTS team_member_history (
-                        history_id INTEGER PRIMARY KEY,
-                        member_id INTEGER,
-                        old_team_id INTEGER,
-                        new_team_id INTEGER,
-                        change_date DATETIME NOT NULL,
-                        FOREIGN KEY (member_id) REFERENCES team_members (member_id),
-                        FOREIGN KEY (old_team_id) REFERENCES teams (team_id),
-                        FOREIGN KEY (new_team_id) REFERENCES teams (team_id)
-                    )
-                ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS schedule (
+            schedule_id INTEGER PRIMARY KEY,
+            team_id INTEGER,
+            shift_type_id INTEGER,
+            date DATE NOT NULL,
+            FOREIGN KEY (team_id) REFERENCES teams (team_id)
+            FOREIGN KEY (shift_type_id) REFERENCES shift_types (shift_type_id)
+        )
+    ''')
 
-        self.conn.commit()
+    cursor.execute('''
+                CREATE TABLE IF NOT EXISTS member_shifts (
+                    member_shift_id INTEGER PRIMARY KEY,
+                    schedule_id INTEGER,
+                    member_id INTEGER,
+                    actual_start_time DATETIME,
+                    actual_end_time DATETIME,
+                    notes TEXT,
+                    FOREIGN KEY (schedule_id) REFERENCES schedule (schedule_id),
+                    FOREIGN KEY (member_id) REFERENCES team_members (member_id)
+                )
+            ''')
+
+    cursor.execute('''
+                CREATE TABLE IF NOT EXISTS team_member_history (
+                    history_id INTEGER PRIMARY KEY,
+                    member_id INTEGER,
+                    old_team_id INTEGER,
+                    new_team_id INTEGER,
+                    change_date DATETIME NOT NULL,
+                    FOREIGN KEY (member_id) REFERENCES team_members (member_id),
+                    FOREIGN KEY (old_team_id) REFERENCES teams (team_id),
+                    FOREIGN KEY (new_team_id) REFERENCES teams (team_id)
+                )
+            ''')
+
+    conn.commit()
+
+
+def get_team_id(team_name):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute('''
+        SELECT team_id FROM teams WHERE team_name = ?
+    ''', (team_name,))
+
+    result = cursor.fetchone()
+
+    return result[0]
+
+
+def generate_schedule(year, month, current_team):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    shift_circle = [1, 2, 3, 3]
+    team_position = get_team_id(current_team)
+
+    _, days_in_month = monthrange(year, month)
+
+    for day in range(1, days_in_month + 1):
+        current_date = date(year, month, day)
+
+        for shift in shift_circle:
+            if team_position > 4:
+                team_position = 1
+
+            team_id = team_position
+            shift_type_id = shift
+
+            cursor.execute('''
+                INSERT INTO schedule (team_id, shift_type_id, date)
+                VALUES (?, ?, ?)
+            ''', (team_id, shift_type_id, current_date))
+
+            team_position += 1
+
+        shift_circle.append(shift_circle.pop(0))
+
+    conn.commit()
+
+def show_schedule():
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute('''
+        SELECT s.date, st.name, t.team_name 
+        FROM
+            schedule as s 
+        JOIN 
+            shift_types as st
+        ON
+            st.shift_type_id = s.shift_type_id
+        JOIN 
+            teams as t 
+        ON 
+            t.team_id = s.team_id
+    ''')
+
+    result = cursor.fetchall()
+    return result
+
+def print_result():
+    result = show_schedule()
+    grouped_schedule = {}
+
+    for item in result:
+        shift_date, shift_type, team = item
+
+        if shift_date not in grouped_schedule:
+            grouped_schedule[shift_date] = []
+        grouped_schedule[shift_date].append([shift_type, team])
+
+    return grouped_schedule
+
+for key, value in print_result().items():
+    team = ''
+    shift = ''
+    print('Дата       |------Смяна----|')
+    for item in value:
+        shift += item[0]
+        team += item[1]
+
+    team = ' | '.join(list(team))
+    shift = ' | '.join(list(shift))
+
+    print(f'########## | {team} | \n{key} | {shift} |')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- main.py
+++ main.py
@@ -1,22 +1,41 @@
 import customtkinter as ctk
+import database.queries as db
+
+root = ctk.CTk()
+ctk.set_appearance_mode('dark')
+ctk.set_default_color_theme('dark-blue')
+
+root.title('Shift Schedule Manager')
+root.geometry('1300x800')
+
+control_frame = ctk.CTkFrame(root)
+control_frame.pack(side='left', fill='y', padx=10, pady=10)
 
 
-class ShiftSchedulerGUI(ctk.CTk):
-    def __init__(self):
-        super().__init__()
+def generate_schedule():
+    year = int(choose_year.get())
+    month = int(choose_month.get())
+    current_team = choose_team.get()
 
-        # Configure the appearance
-        ctk.set_appearance_mode("dark")
-        ctk.set_default_color_theme("blue")
-
-        self.title("Shift Schedule Manager")
-        self.geometry("1300x800")
+    db.generate_schedule(year, month, current_team)
 
 
-def main():
-    app = ShiftSchedulerGUI()
-    app.mainloop()
+choose_year_label = ctk.CTkLabel(control_frame, text='Изберете година')
+choose_year_label.pack(pady=(20, 0))
+choose_year = ctk.CTkComboBox(control_frame, values=['2024', '2025', '2026'])
+choose_year.pack(pady=10)
 
+choose_month_label = ctk.CTkLabel(control_frame, text='Изберете месец')
+choose_month_label.pack(pady=(20, 0))
+choose_month = ctk.CTkComboBox(control_frame, values=['1', '2', '3', '4', '5'])
+choose_month.pack(pady=10)
 
-if __name__ == "__main__":
-    main()
+choose_team_label = ctk.CTkLabel(control_frame, text='Изберете смяна')
+choose_team_label.pack(pady=(20, 0))
+choose_team = ctk.CTkComboBox(control_frame, values=['А', 'Б', 'В', 'Г'])
+choose_team.pack(pady=10)
+
+generate_button = ctk.CTkButton(control_frame, text='Generate', command=generate_schedule)
+generate_button.pack(pady=10)
+
+root.mainloop()
